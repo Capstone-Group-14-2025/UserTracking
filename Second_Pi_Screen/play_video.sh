93c7805f5f642224fe569cb84e3f9cfc9b5705f0
Habiba Mahmoud <74428195+habibaaarahman@users.noreply.github.com>
2025-01-30 18:42:03 -0600

    Update play_video.sh

--- Second_Pi_Screen/play_video.sh
+++ Second_Pi_Screen/play_video.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 cvlc --loop /home/comrade/Downloads/output.mp4
+
+#this is launched bc of /home/comrade/.config/autostart/play_video.desktop *           
+
+## 
+##~/.config/autostart/play_video.desktop
+
+##  GNU nano 7.2   /home/comrade/.config/autostart/play_video.desktop             
+##[Desktop Entry]
+##Type=Application
+##Name=Play Video
+##Exec=cvlc --fullscreen --loop --no-osd /home/comrade/Downloads/output.mp4
+##X-GNOME-Autostart-enabled=true
+## 
